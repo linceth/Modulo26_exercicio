4e71e2e520817c1a15e77b622ed349af278edbeb
Thiago Carvalho <thpc14@gmail.com>
2023-06-30 00:00:32 -0300

    first commit

---
+++ funcao.js
@@ -0,0 +1,7 @@
+"use strict";
+let multiplicacao = (num1, num2) => num1 * num2;
+console.log(multiplicacao(10, 50));
+let saudacao = (nome, saudacao) => `${saudacao} amigos meu nome é ${nome}`;
+console.log(saudacao("Miguel", "Olá"));
+let reverencia = (nome) => ("Olá " + nome);
+console.log(reverencia("Miguel"));
---
+++ funcao.ts
@@ -0,0 +1,14 @@
+let multiplicacao = (num1: number, num2:number):number => num1 * num2;
+
+console.log(multiplicacao(10, 50));
+
+
+let saudacao = (nome: string, saudacao: string):string => `${saudacao} amigos meu nome é ${nome}`;
+
+console.log(saudacao("Miguel", "Olá"));
+
+
+let reverencia = (nome:string):string => ("Olá " + nome);
+
+console.log(reverencia("Miguel" ));
+
